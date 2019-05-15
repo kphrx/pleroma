acc5dfdf0a8bfc13ca13b14ee933d9e97213dc88
kPherox <admin@mail.kr-kp.com>
2019-05-16 02:13:10 +0900

    Turn off app_account_creation

--- config/custom.exs
+++ config/custom.exs
@@ -4,6 +4,8 @@ config :pleroma, :instance,
   registrations_open: false,
   managed_config: false
 
+config :pleroma, :app_account_creation, enabled: false
+
 config :pleroma, :frontend_configurations,
   pleroma_fe: false,
   masto_fe: %{
