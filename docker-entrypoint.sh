ab079326f4759466d92b87a4d61af1a81f5fcb19
kPherox <admin@mail.kr-kp.com>
2020-05-24 19:20:55 +0000

    Changeable user/group id
    Add package: shadow, su-exec

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,13 +2,19 @@
 
 set -e
 
+groupmod -g ${GROUP_ID:-101} pleroma
+usermod -u ${USER_ID:-100} pleroma
+
+chown -R pleroma: /var/lib/pleroma
+chown -R pleroma: /etc/pleroma
+
 echo "-- Waiting for database..."
 while ! pg_isready -U ${DB_USER:-pleroma} -d postgres://${DB_HOST:-db}:5432/${DB_NAME:-pleroma} -t 1; do
     sleep 1s
 done
 
 echo "-- Running migrations..."
-$HOME/bin/pleroma_ctl migrate
+su-exec pleroma $HOME/bin/pleroma_ctl migrate
 
 echo "-- Starting!"
-exec $HOME/bin/pleroma start
+exec su-exec pleroma $HOME/bin/pleroma start
