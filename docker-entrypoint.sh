ead1aae2047ba37c3d9e789ec6711606eea49c87
kPherox <admin@mail.kr-kp.com>
2020-05-24 19:20:55 +0000

    Fix user/group id to changeable
    Add package: shadow, su-exec

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,13 +2,19 @@
 
 set -e
 
+usermod -u ${UID:-$(id -u pleroma)} pleroma
+groupmod -g ${GID:-$(id -g pleroma)} pleroma
+
+chown -R pleroma: /var/lib/pleroma
+chown -R pleroma: /etc/pleroma
+
 echo "-- Waiting for database..."
 while ! pg_isready -U ${DB_USER:-pleroma} -d postgres://${DB_HOST:-db}:5432/${DB_NAME:-pleroma} -t 1; do
     sleep 1s
 done
 
 echo "-- Running migrations..."
-$HOME/bin/pleroma_ctl migrate
+sudo -u pleroma $HOME/bin/pleroma_ctl migrate
 
 echo "-- Starting!"
-exec $HOME/bin/pleroma start
+exec sudo -u pleroma $HOME/bin/pleroma start
