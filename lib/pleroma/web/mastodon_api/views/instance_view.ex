soapbox-pub/rebased

Expose markup configuration in InstanceView
Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

lib/pleroma/web/mastodon_api/views/instance_view.ex
@@ -255,7 +255,8 @@ defmodule Pleroma.Web.MastodonAPI.InstanceView do
         birthday_required: Config.get([:instance, :birthday_required]),
         birthday_min_age: Config.get([:instance, :birthday_min_age]),
         migration_cooldown_period: Config.get([:instance, :migration_cooldown_period]),
-        translation: translation_configuration()
+        translation: translation_configuration(),
+        markup: markup()
       },
       stats: %{mau: Pleroma.User.active_user_count()},
       vapid_public_key: Keyword.get(Pleroma.Web.Push.vapid_config(), :public_key),
@@ -325,5 +326,13 @@ defmodule Pleroma.Web.MastodonAPI.InstanceView do
     else
       nil
     end
+  end
+
+  defp markup() do
+    %{
+      allow_inline_images: Config.get([:markup, :allow_inline_images]),
+      allow_headings: Config.get([:markup, :allow_headings]),
+      allow_tables: Config.get([:markup, :allow_tables])
+    }
   end
 end

kphrx/pleroma

Signed-off-by: Nicole Mikołajczyk <git@mkljczk.pl>

lib/pleroma/web/mastodon_api/views/instance_view.ex
@@ -270,7 +270,8 @@ defmodule Pleroma.Web.MastodonAPI.InstanceView do
         post_formats: Config.get([:instance, :allowed_post_formats]),
         birthday_required: Config.get([:instance, :birthday_required]),
         birthday_min_age: Config.get([:instance, :birthday_min_age]),
-        translation: supported_languages()
+        translation: supported_languages(),
+        markup: markup()
       },
       stats: %{mau: Pleroma.User.active_user_count()},
       vapid_public_key: Keyword.get(Pleroma.Web.Push.vapid_config(), :public_key)
@@ -319,6 +320,14 @@ defmodule Pleroma.Web.MastodonAPI.InstanceView do
     %{
       source_languages: source_languages,
       target_languages: target_languages
+    }
+  end
+
+  defp markup() do
+    %{
+      allow_inline_images: Config.get([:markup, :allow_inline_images]),
+      allow_headings: Config.get([:markup, :allow_headings]),
+      allow_tables: Config.get([:markup, :allow_tables])
     }
   end
 end