4d4174c339b0450aab4bb90473ee8285f87936f5
Nicole Mikołajczyk <git@mkljczk.pl>
2025-03-28 18:47:00 +0100

    fix a few typos
    Signed-off-by: Nicole Mikołajczyk <git@mkljczk.pl>

--- lib/pleroma/web/api_spec.ex
+++ lib/pleroma/web/api_spec.ex
@@ -97,7 +97,7 @@ defmodule Pleroma.Web.ApiSpec do
               "Frontend management",
               "Instance configuration",
               "Instance documents",
-              "Instance rule managment",
+              "Instance rule management",
               "Invites",
               "MediaProxy cache",
               "OAuth application management",
