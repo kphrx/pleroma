958d250fe52711e3d69bba0eba77b06075ee8a80
Phantasm <phantasm@centrum.cz>
2025-10-16 15:11:33 +0200

    Elixir 1.19: Fix typing violation on struct updates in Web.ApiSpec.Rend*
        warning: a struct for OpenApiSpex.Cast.Error is expected on struct update:
    
            %OpenApiSpex.Cast.Error{err | name: err.value}
    
        but got type:
    
            dynamic(%{..., name: nil, reason: :invalid_enum})
    
        where "err" was given the type:
    
            # type: dynamic(%{..., name: nil, reason: :invalid_enum})
            # from: lib/pleroma/web/api_spec/render_error.ex:20:45
            %{name: nil, reason: :invalid_enum} = err
    
        when defining the variable "err", you must also pattern match on "%OpenApiSpex.Cast.Error{}".
    
        hint: given pattern matching is enough to catch typing errors, you may optionally convert the struct update into a map update. For example, instead of:
    
            user = some_function()
            %User{user | name: "John Doe"}
    
        it is enough to write:
    
            %User{} = user = some_function()
            %{user | name: "John Doe"}
    
        typing violation found at:
        │
     21 │           %OpenApiSpex.Cast.Error{err | name: err.value}
        │           ~
        │
        └─ lib/pleroma/web/api_spec/render_error.ex:21:11: Pleroma.Web.ApiSpec.RenderError.call/2

--- lib/pleroma/web/api_spec/render_error.ex
+++ lib/pleroma/web/api_spec/render_error.ex
@@ -17,10 +17,10 @@ defmodule Pleroma.Web.ApiSpec.RenderError do
   def call(conn, errors) do
     errors =
       Enum.map(errors, fn
-        %{name: nil, reason: :invalid_enum} = err ->
+        %OpenApiSpex.Cast.Error{name: nil, reason: :invalid_enum} = err ->
           %OpenApiSpex.Cast.Error{err | name: err.value}
 
-        %{name: nil} = err ->
+        %OpenApiSpex.Cast.Error{name: nil} = err ->
           %OpenApiSpex.Cast.Error{err | name: List.last(err.path)}
 
         err ->
