0b871ff1f298d84c7b3c12444ee923bcfb1ac02a
Lain Soykaf <lain@lain.com>
2026-01-17 12:32:10 +0400

    ConfigController: Don't allow updating the whitelist

--- test/pleroma/web/admin_api/controllers/config_controller_test.exs
+++ test/pleroma/web/admin_api/controllers/config_controller_test.exs
@@ -1220,6 +1220,31 @@ defmodule Pleroma.Web.AdminAPI.ConfigControllerTest do
       assert Application.get_env(:not_real, :anything) == "value6"
     end
 
+    test "doesn't allow updating the database_config_whitelist itself", %{conn: conn} do
+      original_whitelist = Pleroma.Config.get(:database_config_whitelist)
+
+      refute ConfigDB.get_by_group_and_key(:pleroma, :database_config_whitelist)
+
+      conn =
+        conn
+        |> put_req_header("content-type", "application/json")
+        |> post("/api/pleroma/admin/config", %{
+          configs: [
+            %{
+              group: ":pleroma",
+              key: ":database_config_whitelist",
+              value: [%{"tuple" => [":pleroma", ":key1"]}]
+            }
+          ]
+        })
+
+      %{"configs" => configs} = json_response_and_validate_schema(conn, 200)
+
+      assert configs == []
+      assert Pleroma.Config.get(:database_config_whitelist) == original_whitelist
+      refute ConfigDB.get_by_group_and_key(:pleroma, :database_config_whitelist)
+    end
+
     test "args for Pleroma.Upload.Filter.Mogrify with custom tuples", %{conn: conn} do
       assert conn
              |> put_req_header("content-type", "application/json")
